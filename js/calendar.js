dccfcecde50426537f978a847dc73daceb54faad
Zoriana Grys <zoryana.hrys@gmail.com>
2015-12-13 16:47:17 +0200

    first commit adaptive main page

---
+++ js/calendar.js
@@ -0,0 +1,83 @@
+//**** calendar ****
+
+function Calendar (id, year, month) {
+    
+    var Dlast = new Date(year,month+1,0).getDate(),
+    D = new Date(year,month,Dlast),
+    DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
+    DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
+    calendar = '<tr>',
+    month=['Січень','Лютий','Березень','Квітень','Травень','Червень','Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'];
+    
+    if (DNfirst != 0) {
+        for (var  i=1; i<DNfirst; i++) calendar += '<td>' + '</td>';
+    }else{
+        for (var  i=0; i<6; i++) calendar+='<td>' + '</td>';
+    }
+    
+    for(var  i=1; i<=Dlast; i++) {
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+            calendar += '<td class="today">' + i + '</td>';
+        }else{
+            calendar += '<td>' + i + '</td>';
+        }
+  
+        if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
+            calendar += '<tr>';
+        }
+}
+    
+    
+for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+    document.querySelector('#'+id+' tbody').innerHTML = calendar;
+    document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
+    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
+    document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
+    
+    if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+        document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</tr>';
+            }
+}
+
+
+Calendar("calendar", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
+    Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)-1);
+}
+
+// переключатель плюс месяц
+document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
+    Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)+1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,104 @@
+//*** aboutPage ***
+
+function aboutPage(){
+    $('#aboutPage').show();
+    $('#mainPage').hide();
+}
+
+function mainPage(){
+    $('#aboutPage').hide();
+    $('#mainPage').show();
+}
+
+
+
+// *** albums of gallery ***
+
+function performance(){
+        $('#backstage').hide();
+        $('#performance').show();
+};
+
+
+function backstage(){
+        $('#performance').hide();
+        $('#backstage').show();
+};
+
+
+//*** subscription ***
+
+function isEmail(email) {
+      var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
+};
+
+
+function mail (e){
+      $('#subscribeForm').hide();
+    
+    var emailval = $('#email').val();
+      if(!isEmail(emailval)) {
+           $('#error').css('display', 'block');
+           
+          setTimeout(function(){
+               $('#error').hide(); 
+               $('#subscribeForm').show();
+              $('#subscribeForm')[0].reset();
+           },2000);
+          
+        } else {
+            $('#success').css('display', 'block');
+
+    }
+  };
+
+
+// **** personal office - menu****
+
+function calendar(){
+     $('#friends').hide();
+     $('#forum').hide();
+     $('#calendar').show();
+}
+
+function friends(){
+     $('#friends').show();
+     $('#forum').hide();
+     $('#calendar').hide();
+}
+
+function forum(){
+     $('#friends').hide();
+     $('#forum').show();
+     $('#calendar').hide();
+}
+
+
+/// ****User pages****
+
+function userPage1(){
+    $('#userPage1').show();
+    $('#userPage2').hide();
+    $('#userPage3').hide();
+}
+
+function userPage2(){
+    $('#userPage1').hide();
+    $('#userPage2').show();
+    $('#userPage3').hide();
+}
+
+function userPage3(){
+    $('#userPage1').hide();
+    $('#userPage2').hide();
+    $('#userPage3').show();
+}
+
+
+//**** reviews ****
+
+$('.carousel').carousel({
+  pause: 'hover',
+    wrap: true
+});
