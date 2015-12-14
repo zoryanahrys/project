b05b7718aa1787170395900e9498407ac3ab0186
Zoriana Grys <zoryana.hrys@gmail.com>
2015-12-14 13:11:39 +0200

    third commit adaptive main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
-
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(8080);
-
-
-
-
 //*** aboutPage ***
 
 function aboutPage(){
